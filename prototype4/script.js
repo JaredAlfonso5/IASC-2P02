21c4e437ece18a3c82b34cd4cd77152f321b3e3c
Jared <yf22wa@brocku.ca>
2026-03-05 22:49:23 -0500

    proto4

---
+++ prototype4/script.js
@@ -0,0 +1,188 @@
+import * as THREE from 'three';
+import * as dat from "lil-gui"
+import {OrbitControls} from "OrbitControls"
+
+
+/*********
+ * SETUP *
+ ********/
+
+//Sizes
+const sizes ={
+    width: window.innerWidth,
+    height: window.innerHeight,
+    aspectRatio: window.innerWidth / window.innerHeight
+}
+
+
+
+//Resizing 
+window.addEventListener('resize' , () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    sizes.aspectRatio = window.innerWidth / window.innerHeight
+    
+    // Update camera
+    camera.aspect = sizes.aspectRatio
+    camera.updateProjectionMatrix()
+
+    //Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+})
+
+
+/**********
+ ** SCENE**  
+***********/
+
+
+// Canvas
+const canvas = document.querySelector('.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color('gray')
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.aspectRatio,
+    0.1,
+    100
+)
+scene.add (camera)
+camera.position.set(0, 12, -20)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+
+//COntrols
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+/***********
+** LIGHTS **
+************/
+//Directional Light
+const directionalLight = new THREE.DirectionalLight(0x404040, 100)
+scene.add(directionalLight)
+
+/************
+ ** MESHES **
+ ***********/
+
+//cube geometry
+const cubeGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+
+const drawCube = (height, color) =>
+{
+    //create cube material
+    const material = new THREE.MeshStandardMaterial({
+        color: new THREE.Color(color)
+    })
+
+    //Create cube
+    const cube = new THREE.Mesh(cubeGeometry, material)
+
+    //Position cube
+    cube.position.x = (Math.random() - 0.5) * 10
+    cube.position.z = (Math.random() - 0.5) * 10
+    cube.position.y = height - 10
+
+    //Randomize cube rotation
+    cube.rotation.x = Math.random() * 2 * Math.PI
+    cube.rotation.z = Math.random() * 2 * Math.PI
+    cube.rotation.y = Math.random() * 2 * Math.PI
+
+    // Add cube to scene
+    scene.add(cube)
+}
+
+//drawCube(0, 'red')
+//drawCube(1, 'green')
+//drawCube(2, 'yellow')
+//drawCube(3, 'blue')
+
+/******
+ * UI *
+ ******/
+//UI
+const ui = new dat.GUI()
+
+/****************** 
+** TEXT ANALYSIS **
+*******************/
+//SourceText
+const sourceText = "The snake slithered throught the alaskan snow. As it slithered through it met many other creatures such as octupuses, ocelots, foxes, ravens, mantises and wolves. Finally, at the end of the snake's journey, he met yet another snake"
+
+//variables
+let parsedText, tokenizedText
+
+// Parse and Tokenize sourceText
+const tokenizeSourceText = () =>
+{
+    //Strip periods and downcase sourceText
+    parsedText = sourceText.replaceAll(".", "").toLowerCase()
+
+    //Tokenize text
+    tokenizedText = parsedText.split(/[^\w']+/)
+}
+
+// find searchTerm in tokenizedText
+const findSearchTermInTokenizedText = (term, color) =>
+{
+    // Use a for loop to go through the tokenizedText array
+    for (let i=0; i < tokenizedText.length; i++)
+    {
+        // if tokenizedText[i] matches our searchTerm, then we draw a cube
+        if(tokenizedText[i] === term) {
+            //convert i into height, which is a value between 0 and 20
+            const height = (100 / tokenizedText.length) * i * 0.2
+
+            //call drawCube function 100 times usinf converted height value
+            for (let a = 0; a < 100; a++)
+            {
+            drawCube(height, color)
+            }
+        }
+    }
+}
+
+tokenizeSourceText()
+findSearchTermInTokenizedText("snake", "green")
+findSearchTermInTokenizedText("the", "black")
+findSearchTermInTokenizedText("snow", "white")
+
+
+
+/******************
+** ANIMATION LOOP**
+*******************/
+
+const clock = new THREE.Clock()
+
+const animation = () =>
+{
+    //Return elapsedTime
+    const elapsedTime = clock.getElapsedTime()
+
+    //Update OrbitControls
+    controls.update()
+
+    // Renderer
+    renderer.render(scene, camera)
+
+    // Request next frame
+    window.requestAnimationFrame(animation)
+}
+
+animation()
